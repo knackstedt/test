392aac325f973bd68949ea964cb08e31ec7f44ff
Andrew G. Knackstedt <knackstedt@outlook.com>
2025-10-19 22:31:03 -0400

    .

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
-npx -y @dynatrace-oss/dynatrace-mcp-server@latest --http --host 0.0.0.0 --port 3000 &
+npx="npx -y @dynatrace-oss/dynatrace-mcp-server@latest --http --host 0.0.0.0 --port 3000"
+nginx="nginx -g 'daemon off;'"
 
-nginx -g 'daemon off;' &
+eval "${nginx}" &
+eval "${npx}" &
+
+wait $PID1
+if (($? != 0)); then
+    echo "ERROR: nginx exited with non-zero exitcode" >&2
+    kill -9 $PID1
+    exitcode=$((exitcode+1))
+fi
 
 # command: ["npx"]
 # args:
