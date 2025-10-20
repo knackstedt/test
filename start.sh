ecd7a177a0e30ce12bfeac66a9b834837ce08ea3
Andrew G. Knackstedt <knackstedt@outlook.com>
2025-10-19 22:27:29 -0400

    .

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-npx -y @dynatrace-oss/dynatrace-mcp-server@latest --http --host 0.0.0.0 --port 3000
+npx -y @dynatrace-oss/dynatrace-mcp-server@latest --http --host 0.0.0.0 --port 3000 &
 
-nginx -g 'daemon off;'
+nginx -g 'daemon off;' &
 
 # command: ["npx"]
 # args:
